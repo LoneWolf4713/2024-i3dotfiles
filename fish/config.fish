3ac93eca14a5316f44ee9af1aec2a3172eb2949e
LoneWolf4713 <72555478+LoneWolf4713@users.noreply.github.com>
2024-01-09 20:15:15 +0530

    dotfiles upload.

---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    oh-my-posh init fish --config ~/.local/bin/rudolfs-dark.omp.json | source
+
+    # Commands to run in interactive sessions can go here
+end
+fish_add_path /home/prtyksh/.spicetify
